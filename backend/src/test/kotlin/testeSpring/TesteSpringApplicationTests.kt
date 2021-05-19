ffc72b6d04fd867015e9bfaa124dd79eaa4fbb3d
lcsm-ecomp <lcsm@ecomp.poli.br>
2021-05-19 16:48:48 +0000

    Backend

---
+++ backend/src/test/kotlin/testeSpring/TesteSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package testeSpring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TesteSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
