8580c991b5c2aab0673797129b3a6cf83d4ce7ce
lcsm-ecomp <lcsm@ecomp.poli.br>
2021-05-19 16:30:32 +0000

    Teste 1

--- src/main/kotlin/testeSpring/TesteSpringApplication.kt
+++ src/main/kotlin/testeSpring/TesteSpringApplication.kt
@@ -15,7 +15,24 @@ var numero = 0
 
 @RestController
 class MessageResource {
-    @GetMapping("/ola.html")
+    @GetMapping("/") 
+    fun paginaInicial() : String { 
+    println("ola para todos")
+    return """
+        <html>
+        <title>Página Inicial do Sistema</title>
+        <body>
+        Este é um exemplo de página gerada por um programa em 
+        kotlin. Esse programa foi executado no servidor web.
+        Apesar de ser gerada por um programa ela é estática<p>
+        <a href="pagina_estatica.html">Outra Página estática</a><p>
+        <a href="pagina_dinamica.html">Página Gerada Dinamicamente</a><p>
+        </body>
+        </html>
+        """
+    }
+    
+    @GetMapping("/pagina_dinamica.html")
     fun old(@RequestParam(value = "name", defaultValue = "todos") name:String): String = """
     <html> <title>Ola</title>
     <body> Ola $name,
